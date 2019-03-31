f5fc03716dd4a988c1874a2f4c1eb92b19775815
Jessica Kincaid <4j.kincaid@gmail.com>
2019-03-30 21:46:48 -0500

    index through artists

--- studio-visitor-app/src/index.js
+++ studio-visitor-app/src/index.js
@@ -12,9 +12,32 @@ serviceWorker.unregister();
 
 const artists = [
     {
-        name: "Lilly",
-        studio: 8, 
-        id: 0
+        id: 4,
+        name: 'Kaia',
+        studio:  5,
+        bio: 'Kaias-120-word-bio'
+        url: 'img/Kaia.jpg'
+      },
+    {
+        id: 5,
+        name: 'Jen',
+        studio:  6,
+        bio: 'Jens-120-word-bio'
+        url: 'img/Jen.jpg'
+      },
+    {
+        id: 6,
+        name: 'Kevin',
+        studio:  7,
+        bio: 'Kevins-120-word-bio'
+        url: 'img/kevin.jpg'
+      },
+    {
+        id: 7,
+        name: 'Lilly',
+        studio:  8,
+        bio: 'Lillys-120-word-bio'
+        url: 'img/lilly.jpg'
       },
       {
         name: "Kiki",
@@ -33,16 +56,6 @@ const artists = [
       }
 ];
 
-
-const Header = (props) => {
-    console.log(props);
-    return (
-        <header>
-            <h1>{props.title}</h1>
-            <span className="stats">Studios: {props.totalArtists}</span>
-        </header>
-    );
-}
 
 const Artist = (props) => {
     return (
@@ -68,7 +81,7 @@ const Counter = (props) => {
 
 const App = (props) => {
     return (
-        <div className="studioboard">
+        <div className="studioList">
             <Header 
             title="Artist" 
             totalArtists = {props.initialArtists.length}/>
@@ -84,6 +97,21 @@ const App = (props) => {
         </div>
     );
 }
+const ArtistList = (props) => {
+    return (
+        <div className="mainMenu">
+
+            {props.artist.map( artist =>
+            <Artist 
+            name={artist.name}
+            studio={artist.studio}
+            key={artist.id}
+            />
+            )}
+        </div>
+    );
+}
+
 
 ReactDOM.render(
     <App initialArtists = {artists}/>,
