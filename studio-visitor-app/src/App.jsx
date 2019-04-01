07415df9553ad49f02db32a33a603b87a2bf6515
Jessica Kincaid <4j.kincaid@gmail.com>
2019-03-31 20:16:45 -0500

    built artist component for each object in the array to cycle through studios

--- studio-visitor-app/src/App.jsx
+++ studio-visitor-app/src/App.jsx
@@ -1,16 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Component } from 'react';
+// import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-<div id="container">
+const artists = [
+    {
+        id: 4,
+        name: 'Kaia',
+        studio:  5,
+        bio: 'Kaias-120-word-bio'
+      },
+    {
+        id: 5,
+        name: 'Jen',
+        studio:  6,
+        bio: 'Jens-120-word-bio'
+      },
+    {
+        id: 6,
+        name: 'Kevin',
+        studio:  7,
+        bio: 'Kevins-120-word-bio'
+      },
+    {
+        id: 7,
+        name: 'Lilly',
+        studio:  8,
+        bio: 'Lillys-120-word-bio'
+      },
+      {
+        name: "Kiki",
+        studio: 9,
+        id: 1
+      },
+      {
+        name: "Jessica",
+        studio: 10,
+        id: 2
+      },
+      {
+        name: "Laurena",
+        studio: 11,
+        id: 3
+      }
+
+];
+
+const Artist = (props) => {
+    return (
+        <div className="profile">
+        <div>
+            <img src={props.url} />
+        </div>
+        <h2>{props.name}</h2>
+        <h2>{props.studio}</h2>
+        <p>{ props.bio }</p>
+            {props.artists.map( artist =>
+            <Artist 
+                {...artist}
+                key={artist.id}
+            />
+            )}
+        </div>
+    );
+}
+const ArtistList = (props) => {
+    return (
+        <div className="container">
+        {props.artists.map( artist => 
+        <Artist
+        {...artist}
+        key={artist.id}
+        />
+        )}
         <header>
             <h1>CHARLOTTE STREET FOUNDATION OPEN STUDIOS EVENT</h1>
         </header>
 
-            <div id="map" class=".image" >
+            <div id="map" className="image" >
             <svg>
                 <defs>
                     <g id="studio-icon">
@@ -24,44 +91,20 @@ class App extends Component {
                 </svg>
             </div>
 
-    
-        <button onclick="visitArtist()" class="dropbtn">Artists</button>
-            <ul id="artistMenu" class="dropdown-content" class="sub-menu">
+    }
+}
 
-                <li class="vis"><a href="#">1 Katerina Guillermo</a></li>
-                <li class="write perf"><a href="profile.html">2 Madison Mae Parker</a></li>
-                <li class="write perf"><a href="#">3 Jessica Ayala</a></li>
-                <li class="write perf"><a href="#">4 Sheri Hall</a></li>
-                <li class="write"><a href="#">6 Lavinia Roberts</a></li>
-                <li class="perf"><a href="#">7 Kaia Nutting</a></li>
-                <li class="write"><a href="#">8 Kevin Kilroy</a></li>
-                <li class="vis"><a href="#">9 Lilly McElroy</a></li>
-                <li class="vis"><a href="#">10 Kiki Serna</a></li>
-                <li class="vis"><a href="#">11 Jessica Kincaid</a></li>
-                <li class="vis"><a href="#">12 Laurena Roytberg</a></li>
-                <li class="perf"><a href="#">13 Jason Zeh</a></li>
-                <li class="perf"><a href="#">14 Tristian Griffin</a></li>
-                <li class="perf"><a href="#">15 Daniel Hogans</a></li>
-                <li class="perf"><a href="#">16 KC Public Theatre</a></li>
-                <li class="perf"><a href="#">17 Mazzy Mann</a></li>
-                <li class="vis"><a href="#">18 JE Baker</a></li>
-                <li class="vis"><a href="#">19 Fuko Ito</a></li>
-                <li class="vis"><a href="#">20 Elizabeth Stehling</a></li>
-                <li class="write"><a href="#">21 Alicen Lundberg</a></li>
-                <li class="vis"><a href="#">22 Luke Haynes</a></li>
-                <li class="vis"><a href="#">23 Benjamin Wills</a></li>
-                <li class="vis"><a href="#">24 Iliann Alvarez</a></li>
-                <li class="vis"><a href="#">25 Elizabeth Derstine</a></li>
-                <li class="vis"><a href="#">26 Ruben Castillo</a></li>
-                <li class="vis"><a href="#">27 Bo Hubbard</a></li>
-                <li class="vis"><a href="#">28 Nazanin Amiri</a></li>
-            </ul>
+    
     
             <footer><small>&copy; 2019 Jessica Kincaid</small></footer>
         </div>
     
         );
     }
-}
 
-export default App;
+   
+
+ReactDOM.render(
+<ArtistList artists={artists} />,
+document.getElementById('root')
+);
