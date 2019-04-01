757726a5f25e580d33e837351304dd4b8e5d3dfe
Jessica Kincaid <4j.kincaid@gmail.com>
2019-04-01 13:13:31 -0500

    added profile images, made props in array match props in Artist component

--- studio-visitor-app/src/App.jsx
+++ studio-visitor-app/src/App.jsx
@@ -1,109 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Component } from 'react';
+import { Component } from 'react';
 // import './App.css';
 
 const artists = [
     {
-        id: 4,
-        name: 'Kaia',
-        studio:  5,
-        bio: 'Kaias-120-word-bio'
-      },
-    {
-        id: 5,
-        name: 'Jen',
-        studio:  6,
-        bio: 'Jens-120-word-bio'
-      },
-    {
-        id: 6,
-        name: 'Kevin',
-        studio:  7,
-        bio: 'Kevins-120-word-bio'
-      },
-    {
         id: 7,
         name: 'Lilly',
-        studio:  8,
-        bio: 'Lillys-120-word-bio'
+        studio:  9,
+        bio: 'Lillys-120-word-bio',
+        type: 'visual', 
+        url: 'images/09lilly.jpg'
       },
       {
-        name: "Kiki",
-        studio: 9,
-        id: 1
-      },
-      {
+        id: 9,
         name: "Jessica",
-        studio: 10,
-        id: 2
+        studio: 11,
+        bio: 'Jessicas-120-word-bio',
+        type: 'visual',
+        url: 'images/11jessica.jpg'
       },
       {
+        id: 10,
         name: "Laurena",
-        studio: 11,
-        id: 3
+        studio: 12,
+        bio: 'Laurenas-120-word-bio',
+        type: 'visual',
+        url: 'images/12laurena.jpg'
       }
 
 ];
 
 const Artist = (props) => {
     return (
-        <div className="profile">
-        <div>
-            <img src={props.url} />
-        </div>
-        <h2>{props.name}</h2>
-        <h2>{props.studio}</h2>
+      <div className="profile">
+      <div>
+        <img src={props.url} alt={props.picture} />
+      </div>
+        <h2>{ props.name }</h2>
+        <p>{ props.studio }</p>
         <p>{ props.bio }</p>
-            {props.artists.map( artist =>
-            <Artist 
-                {...artist}
-                key={artist.id}
-            />
-            )}
-        </div>
+        <h3>Artist Profile</h3>
+        <ul>
+          <li><strong>Type:</strong> { props.type }</li>
+        </ul>
+      </div>
     );
-}
+  }
+
+
 const ArtistList = (props) => {
     return (
-        <div className="container">
-        {props.artists.map( artist => 
-        <Artist
-        {...artist}
-        key={artist.id}
-        />
+      <div className="container">
+        {props.artist.map( artist => 
+          <Artist
+            {...artist}
+            key={artist.id}
+          />      
         )}
-        <header>
-            <h1>CHARLOTTE STREET FOUNDATION OPEN STUDIOS EVENT</h1>
-        </header>
-
-            <div id="map" className="image" >
-            <svg>
-                <defs>
-                    <g id="studio-icon">
-                       
-                    </g>
-
-                    <g id="studio-map">
-                       
-                    </g>
-                </defs>
-                </svg>
-            </div>
-
-    }
-}
-
-    
-    
-            <footer><small>&copy; 2019 Jessica Kincaid</small></footer>
-        </div>
-    
-        );
-    }
-
-   
-
+      </div>
+    );
+  }
+  
 ReactDOM.render(
 <ArtistList artists={artists} />,
 document.getElementById('root')
--- studio-visitor-app/src/index.js
+++ studio-visitor-app/src/index.js
@@ -1,6 +1,6 @@
 
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // If you want your app to work offline and load faster, you can change
